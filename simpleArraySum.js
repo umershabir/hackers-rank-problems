e77733e569d2f4b4633d88047656b9ac256bc367
Umer Shabir <muhammadumershabir@gmail.com>
2023-01-30 00:32:44 +0500

    simple array sum challenge completed

---
+++ simpleArraySum.js
@@ -0,0 +1,14 @@
+const sampleArr = [1, 2, 3, 4, 5, 6];
+// the answer is 21
+
+function simpleArraySum(arr) {
+  console.log(arr.length);
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let currentVal = arr[i];
+    sum += currentVal;
+  }
+  return sum;
+}
+
+console.log(simpleArraySum(sampleArr));

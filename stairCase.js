5d346f81015632f9bdca2d04f2c5c674ea32bea8
Umer <muhammadumershabir@gmail.com>
2023-03-13 07:37:11 +0500

    trying staircase problem

---
+++ stairCase.js
@@ -0,0 +1,12 @@
+let stair =4
+
+function stairCase(st){
+    let stairCase ='#';
+    for(let i = 0; i < st; i++) {
+        console.log(stairCase)
+        stairCase += '#'
+    }
+    
+}
+
+stairCase(stair)

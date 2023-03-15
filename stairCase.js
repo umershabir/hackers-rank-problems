91efcc38ab55f7608c649ccc2805187399bf6b96
Umer <muhammadumershabir@gmail.com>
2023-03-15 07:52:43 +0500

    stair case

--- stairCase.js
+++ stairCase.js
@@ -1,10 +1,13 @@
 let stair =4
 
 function stairCase(st){
-    let stairCase ='#';
-    for(let i = 0; i < st; i++) {
-        console.log(stairCase)
-        stairCase += '#'
+    let stairCase = ''
+    for(let j = st; j > 0; j--){
+        let space = ' '.repeat(j)   
+        for(let i = 0; i < st; i++) {
+            stairCase  += '#'
+            console.log(space+stairCase)
+        }
     }
     
 }
